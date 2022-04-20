19b7964fad4b2e9b9dfdb88764b7f5c0123720ca
Pajehome <abdel20069@gmail.com>
2022-04-20 15:52:02 +0200

    fatto

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const hello = new Vue({
     el: '#hello',
     data: {
         testo:'Benvenuto',
-        immagine: 'img/slap1.png',
+        immagine: '',
         sfondo: 'bg',
         isActive: false
     },
     methods: {
         addImg() {
-            
+
             if(!this.isActive){
             this.isActive = true;
             this.immagine = 'img/slap.png';
