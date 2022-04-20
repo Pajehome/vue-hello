2c7feed70b0846af3c568b9cdc3262f432755876
Pajehome <abdel20069@gmail.com>
2022-04-20 15:38:37 +0200

    change

--- main.js
+++ main.js
@@ -4,10 +4,19 @@ const hello = new Vue({
         testo:'Benvenuto',
         immagine: 'img/slap1.png',
         sfondo: 'bg',
+        isActive: false
     },
     methods: {
         addImg() {
+            if(!this.isActive){
+            this.isActive = true;
             this.immagine = 'img/slap.png';
-        }
+            }else {
+            this.isActive = false;
+            this.immagine = 'img/slap1.png';
+            }
+            
+        },
+
     }
 })
