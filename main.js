caceec783a7295b8b07bdf6bed3a0ced6190fc29
Pajehome <abdel20069@gmail.com>
2022-04-20 15:43:24 +0200

    semplicementeVue

--- main.js
+++ main.js
@@ -8,14 +8,16 @@ const hello = new Vue({
     },
     methods: {
         addImg() {
+            
             if(!this.isActive){
             this.isActive = true;
             this.immagine = 'img/slap.png';
+            this.testo = 'Ti avevo detto di non cliccare'
             }else {
             this.isActive = false;
             this.immagine = 'img/slap1.png';
-            }
-            
+            this.testo = 'Riprovaci'
+            } 
         },
 
     }
